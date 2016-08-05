879c8b5ec29255e770268a9e8debd684f6e7447c
pierrick-succubus <pierrick.pasutto@succubus.fr>
2016-08-05 16:09:14 +0200

    First commit avec une première scène nulle

---
+++ PickupGame/Assets/scripts/Hero.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero : MonoBehaviour {
+
+    // Use this for initialization
+    public float speed = 1.5f;
+
+	void Start () {
+        Debug.Log("Hello World !");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(0, 0, speed) * Time.deltaTime;
+	}
+}
